13f0ae06c12ced5581cfca0f7c3daba4e9283210
tomjohnneill <tomjohnneill@live.co.uk>
2021-04-08 19:33:46 +0100

    default images and filter by books more than 1

--- components/TopSets.js
+++ components/TopSets.js
@@ -3,6 +3,13 @@ import { useContext, useEffect, useState } from "react";
 import { supabase } from "../lib/initSupabase";
 import UserContext from "../lib/UserContext";
 
+const defaultImages = [
+  "https://images.unsplash.com/photo-1513680592398-887abb03c760?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+  "https://images.unsplash.com/photo-1528227153184-d310240280ed?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80",
+  "https://images.unsplash.com/photo-1596123068611-c89d922a0f0a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1022&q=80",
+  "https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1052&q=80",
+];
+
 const TopSets = ({}) => {
   const [setList, setSetList] = useState([]);
 
@@ -18,14 +25,19 @@ const TopSets = ({}) => {
         emoji,
         creator,
         description,
-        image
-      `
+        image,
+        book_views (
+          id
+        )
+      `,
+        { count: "exact" }
       )
       .order("id", true);
     if (error) {
       alert(error.message);
     } else {
-      setSetList(sets);
+      console.log({ sets });
+      setSetList(sets.filter((set) => set.book_views.length > 0));
     }
   };
 
@@ -33,10 +45,13 @@ const TopSets = ({}) => {
     fetchData();
   }, []);
 
-  return setList.map((set) => (
+  return setList.map((set, i) => (
     <Link href={`/set/${set.id}`}>
       <a className="max-w-xl w-full rounded-lg overflow-hidden border border-gray-200">
-        <img className="w-full h-32 object-cover" src={set.image} />
+        <img
+          className="w-full h-32 object-cover"
+          src={set.image || defaultImages[i % defaultImages.length]}
+        />
         <div className="p-4">
           <div className="font-bold">
             {set.emoji} {set.name}{" "}
